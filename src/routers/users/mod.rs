2c59905c2f461f7cc6d6f986975858643bd654f6
ka.watcharin21 <ka.watcharin21@gmail.com>
2024-06-13 00:22:10 +0700

    Edit routers users.

--- src/routers/users/mod.rs
+++ src/routers/users/mod.rs
@@ -1,19 +1,85 @@
-mod auth;
-mod register;
-
-use auth::apis::authentication::router as auth_router;
-use register::apis::{
-    users::router as users_router,
-    register::router as register_router
-};
-
-use axum::Router;
+use axum::{routing::{get, post}, Router, Json, http::StatusCode};
+use axum::response::{IntoResponse, Response};
+use serde_json::{json, Value};
+use crate::errors::auth::{ Result, Error };
 
 
-/* Main Router: Users */ 
 pub fn router() -> Router {
     Router::new()
-        .nest("/tests", users_router())  
-        .nest("/register", register_router())
-        .nest("/auth", auth_router())  
+        .nest("/login", login::router())
+        .nest("/users", users::router())
+}
+
+
+mod login {
+    use super::*;
+    use crate::models::auth::login::{ LoginPayload, LoginResponse };
+    use crate::services::auth::login::check as check_login;
+
+
+    pub fn router() -> Router {
+        Router::new()
+            .route("/check", post(check))
+            .route("/test", post(test_res))
+            .route("/handler", get(handler))
+            .route("/test_err", get(test_err))
+    }
+
+
+    async fn check(Json(payload): Json<LoginPayload>) -> Result<(StatusCode, Json<LoginResponse>)> {
+        check_login(payload)
+    }
+
+    async fn test_res(Json(payload): Json<LoginPayload>) -> Result<impl IntoResponse> {
+        if payload.username == "" && payload.pwd == "" {
+            Err(Error::LoginFailed)
+        } else {
+            Ok(
+                (
+                    StatusCode::OK,
+                    Json(json!({ "message": "successfull"}))
+                )
+            )
+        }
+    }
+
+    async fn handler() -> Response {
+        let response = (
+            StatusCode::OK,
+            Json(json!({ "detail": "test" }))
+        );
+        response.into_response()
+    }
+
+    async fn test_err() -> Result<()> {
+        Err(Error::DatabaseQueryError)
+    }
+}
+
+
+mod users {
+    use super::*;
+    use crate::models::users::users::{GetRequest, GetResponse};
+    use crate::services::users::users::test_loop_vecs;
+
+
+    pub fn router() -> Router {
+        Router::new()
+            .route("/get", get(get_user))
+            .route("/post", post(post_user))
+    }
+
+    
+    async fn get_user() -> Json<GetResponse> {
+        Json(GetResponse {
+            detail: "detail".to_string()
+        })
+    }
+
+    async fn post_user(Json(paylaod): Json<GetRequest>) -> Json<Value> {
+        test_loop_vecs();
+        Json(json!(GetResponse {
+            detail: paylaod.name
+        }))
+    }
 }
