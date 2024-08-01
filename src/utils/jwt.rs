118c18016893b32c1d5fefe1b1a9c632aec55b53
ka.watcharin21 <ka.watcharin21@gmail.com>
2024-08-01 17:46:00 +0700

    Add structure file and jwt.

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,32 +1,53 @@
-use serde::{Serialize, Deserialize};
-#[allow(unused)]
-use jsonwebtoken::{
-    encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey
+#![allow(unused)]
+use jsonwebtoken::{encode, Header, Algorithm, EncodingKey};
+use serde::Deserialize;
+use serde::Serialize;
+use url_encoded_data::stringify;
+use serde_json::{json, Value};
+use chrono::{prelude::*, Duration};
+use dotenv::dotenv;
+use std::env;
+use std::fmt;
+use std::fs;
+
+use crate::configs::env::main::{
+    SECRET_KEY, SERVICE_ACCOUNT, CLIENT_ID, CLIENT_SECRET
 };
-use chrono::{Duration, Utc};
-#[allow(unused)]
-use crate::configs::env::main::SECRET_KEY;
 
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+pub type Result<T, E = Box<dyn std::error::Error>> = core::result::Result<T, E>;
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
-    pub username: String,
-    pub password: String,
-    pub datetime_now: i64,
-    pub datetime_exp: i64,
+    pub iss: u8,
+    pub sub: String,
+    pub iat: i64,
+    pub exp: i64
 }
 
 impl Claims {
-    #[allow(unused)]
-    pub fn new(username: String, password: String) -> Self {
-        let datetime_now = Utc::now();
-        let datetime_exp = datetime_now + Duration::hours(1);
-
+    pub fn new(iss: u8, sub: String, iat: i64, exp: i64) -> Self {
         Self {
-            username,
-            password,
-            datetime_now: datetime_now.timestamp(),
-            datetime_exp: datetime_exp.timestamp()
+            iss, sub, iat, exp
         }
+    }
+
+    pub fn make_jwt() -> Result<String, Box<dyn std::error::Error>>{
+        let mut header = Header::new(Algorithm::RS256); 
+        header.typ = Some("JWT".to_string());
+
+        let now = Local::now();
+        let iat = now.timestamp();
+        let exp = (now + Duration::hours(1)).timestamp();
+
+        let claims = Self::new(
+            CLIENT_ID.clone(), 
+            SERVICE_ACCOUNT.clone(), 
+            iat, 
+            exp
+        );
+
+        let jwt = encode(&header, &claims, &EncodingKey::from_rsa_pem( SECRET_KEY.clone().as_bytes())?)?;
+        Ok(jwt)
     }
 }
